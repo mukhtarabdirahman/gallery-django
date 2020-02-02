860e7766d6615e192c1c837dcd16fa14c4014398
mukhtarabdirahman <mukhtarabdirahmanm@gmail.com>
2020-02-02 09:39:08 +0300

    first commit

---
+++ pics/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse, Http404
+import datetime as dt
+# Create your views here.
+def welcome(request):
+    return render(request, 'welcome.html')
+
+def pics_of_day(request):
+    date = dt.date.today()
+    return render(request, 'all-pics/today-pics.html', {"date": date,})
+def convert_dates(dates):
+    
+    # Function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day
